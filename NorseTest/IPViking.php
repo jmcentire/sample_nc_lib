7d16c4ee680eb2bdb8e588b2ba9858637641ebb0
Jeremy McEntire <jm@norse-corp.com>
2013-08-20 17:49:45 -0700

    Getting composer to work.

--- NorseTest/IPViking.php
+++ NorseTest/IPViking.php
@@ -1,9 +1,15 @@
 <?php
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
 class IPVikingTest extends PHPUnit_Framework_TestCase {
 
     public function testPHPUnit() {
         return true;
+    }
+
+    public function testAutoLoad() {
+        $this->assertInstanceOf('Exception', new Norse\IPViking\Exception());
     }
 
 }
